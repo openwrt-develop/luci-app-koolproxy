effc5bbfe47779950f76b0093401e97be4cad60b
panda-mute <wxuzju@gmail.com>
2017-04-25 17:22:02 +0800

    Update binary to 3.5.2/rules/host and update script self

--- koolproxyupdate.sh
+++ koolproxyupdate.sh
@@ -1,9 +1,13 @@
-wget 'https://raw.githubusercontent.com/koolproxy/koolproxy_rules/master/1.dat' -O files/usr/share/koolproxy/data/rules/1.dat
+wget 'https://raw.githubusercontent.com/koolproxy/koolproxy_rules/master/kp.dat' -O files/usr/share/koolproxy/data/rules/kp.dat
 wget 'https://raw.githubusercontent.com/koolproxy/koolproxy_rules/master/koolproxy.txt' -O files/usr/share/koolproxy/data/rules/koolproxy.txt
 wget 'https://raw.githubusercontent.com/koolproxy/koolproxy_rules/master/user.txt' -O files/usr/share/koolproxy/data/rules/user.txt
-wget 'https://github.com/koolproxy/koolproxy_rules/blob/master/downloads/arm?raw=true' -O files/bin/arm
-wget 'https://github.com/koolproxy/koolproxy_rules/blob/master/downloads/i386?raw=true' -O files/bin/i386
-wget 'https://github.com/koolproxy/koolproxy_rules/blob/master/downloads/mips?raw=true' -O files/bin/mips
-wget 'https://github.com/koolproxy/koolproxy_rules/blob/master/downloads/mipsel?raw=true' -O files/bin/mipsel
-wget 'https://github.com/koolproxy/koolproxy_rules/blob/master/downloads/x86_64?raw=true' -O files/bin/x86_64
+wget 'https://koolproxy.com/downloads/arm' -O files/bin/arm
+wget 'https://koolproxy.com/downloads/i386' -O files/bin/i386
+wget 'https://koolproxy.com/downloads/mips' -O files/bin/mips
+wget 'https://koolproxy.com/downloads/mipsel' -O files/bin/mipsel
+wget 'https://koolproxy.com/downloads/x86_64' -O files/bin/x86_64
 chmod +x files/bin/*
+
+wget https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt -O- | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > files/usr/share/koolproxy/dnsmasq.adblock
+sed -i '/youku/d' files/usr/share/koolproxy/dnsmasq.adblock
+sed -i '/[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}/d' files/usr/share/koolproxy/dnsmasq.adblock
